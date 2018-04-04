29be04b42a40c1eabd21c906100bcd135adff98d
Nina <nina.poerner@gmail.com>
2018-04-04 11:45:00 +0200

    .

---
+++ lime_layers.py
@@ -0,0 +1,195 @@
+import keras.backend as K
+import tensorflow as tf
+import numpy as np
+from explanation_layers import ExplanationLayer
+
+class LIME(ExplanationLayer):
+    def __init__(self, layer, samples, **kwargs):
+        super(LIME, self).__init__(layer, **kwargs)
+        self.samples = samples
+
+    def compute_output_shape(self, input_shape):
+        output_shape = self.layer.compute_output_shape(input_shape)
+        return input_shape[:2] + output_shape[1:]
+
+    def call(self, inputs, mask = None):
+        # _sample is implemented by child classes
+        # since one instantiation of LIME can handle samples of different sizes,
+        # the child must return a list of tensors:
+        # [(batchsize, samples, timesteps, ...)]
+        samples_by_size, binaries_by_size, masks_by_size = self._sample(inputs, mask)
+        minus_one = (-1) * K.ones((1,), dtype = K.dtype(K.shape(inputs)))
+
+        def flatten_01(x):
+            return K.reshape(x, K.concatenate([minus_one, K.shape(x)[2:]]))
+
+        samples_by_size_flat = [flatten_01(_x) for _x in samples_by_size]
+        # (batchsize * samples, timesteps...)
+
+        if mask:
+            masks_by_size_flat = [flatten_01(_m) for _m in masks_by_size]       
+            preds_by_size_flat = [self.layer.call(_x, mask = _m)\
+                    for _x, _m in zip(samples_by_size_flat, masks_by_size_flat)]
+        
+        else:
+            preds_by_size_flat = [self.layer.call(_x) for _x in samples_by_size_flat]
+        
+        # [(batchsize * samples, output...)]
+        preds_by_size = [K.reshape(_p, K.concatenate([K.shape(inputs)[:1], minus_one, K.shape(_p)[1:]], axis = 0)) \
+                for _p in preds_by_size_flat]
+        
+        preds = K.concatenate(preds_by_size, axis = 1)
+        # (batchsize, samples, output_shape)
+        
+        preds_shape = K.shape(preds)
+        preds = K.reshape(preds, K.concatenate([preds_shape[:2], minus_one], axis = 0))
+        binary = K.concatenate(binaries_by_size, axis = 1)
+        # (batchsize, samples, timesteps)
+
+        def solve(idx):
+            squared_matrix = K.dot(binary[idx], K.transpose(binary[idx]))
+            inverted = tf.py_func(np.linalg.pinv, [squared_matrix], Tout = K.floatx())
+            tmp = K.dot(inverted, binary[idx])
+            return K.dot(K.transpose(tmp), preds[idx])
+
+        weights = K.map_fn(lambda idx:solve(idx), K.arange(0, K.shape(inputs)[0]), dtype = K.floatx())
+        # (batchsize, timesteps, output_shape_flat)
+
+        weights = K.reshape(weights, K.concatenate([K.shape(weights)[:2], preds_shape[2:]], axis = 0))
+
+        # cut off weights associated with the padded material
+        return self._finalize(weights, inputs)
+
+class StandardLIME(LIME):
+    def __init__(self, layer, samples, size, axis, **kwargs):
+        super(StandardLIME, self).__init_(layer, samples, **kwargs)
+        self.axis = axis
+        self.size = size
+
+    def _pad(self, x):
+        return x
+
+    def _finalize(self, weights, inputs):
+        return weights
+        
+    def _sample(self, inputs, mask=None):
+        pattern = [self.axis] + [i if i != self.axis else 0 for i in range(1, K.ndim(inputs))]
+        
+        inputs_padded = self._pad(inputs) 
+        inputs_perm = K.permute_dimensions(inputs_padded, pattern)
+        if mask and self.axis <= 1:
+            mask_padded = self._pad(mask)
+            mask_perm = K.permute_dimensions(mask_padded, pattern)
+
+        batchsize = K.shape(inputs_padded)[0]
+        timesteps = K.shape(inputs_padded)[self.axis]
+        
+        def sample_positions(_):
+            return K.random_uniform(shape = (self.size, batchsize), minval=0, maxval=timesteps-self.size, dtype = "int32")
+
+        positions = K.map_fn(get_sample, K.arange(0, self.samples), dtype = "int32")
+        # (self.samples, self.size, batchsize)
+
+        def sample_sample(positions):
+            return K.permute_dimensions(inputs_perm[positions], pattern)
+            
+        samples = [K.map_fn(sample_sample, positions, dtype = K.dtype(inputs))]
+
+        def sample_binary(positions):
+            tmp = K.sum(K.one_hot(positions, timesteps), axis = 0) # count times every 
+            tmp = K.cast(K.cast(tmp, "bool"), "int32") # avoid counts > 0
+            return tmp
+        
+        binaries = [K.map_fn(sample_binary, positions, dtype = K.dtype(inputs))]
+
+        if mask and self.axis <= 1:
+            def sample_mask(positions):
+                return K.permute_dimensions(mask_perm[positions], pattern)
+
+            masks = [K.map_fn(sample_mask, positions, dtype = K.dtype(mask))]
+
+        else:
+            masks = []
+
+        return samples, binaries, masks
+
+        
+class LIMSSE(LIME):
+    def __init__(self, layer, samples, low=1, high=8, **kwargs):
+        super(LIMSSE, self).__init__(layer, samples, **kwargs)
+        if low < 1 or high <= low:
+            raise Exception("0 < low < high")
+
+        if samples < high - low:
+            raise Exception("there must be more samples than high - low")
+
+        self.high = high
+        self.low = low
+
+    def _pad(self, x):
+        padding_length = K.max(K.stack([self.high - K.shape(x)[1], 0], axis = 0))
+        padding_shape = K.concatenate([K.stack([K.shape(x)[0], padding_length], 0), K.shape(x)[2:]], 0)
+        return K.concatenate([x, K.zeros(padding_shape)], axis = 1)
+
+    def _finalize(self, weights, inputs):
+        return weights[:,:K.shape(inputs)[1]]
+
+    def _sample(self, inputs, mask = None):
+        
+        substrings_by_size = []
+        binaries_by_size = []
+        masks_by_size = []
+    
+        inputs_padded = self._pad(inputs)
+        if mask:
+            mask_padded = self._pad(mask)
+        
+        batchsize = K.shape(inputs_padded)[0]
+        timesteps = K.shape(inputs_padded)[1]
+        
+        def sample_length(_):
+            return K.random_uniform(shape = (1,), minval=self.low, maxval=self.high, dtype = "int32")
+        
+        all_lengths = K.arange(self.low, self.high)
+        samples_left = self.samples - (self.high - self.low) 
+        
+        all_lengths = K.concatenate([all_lengths, 
+            K.squeeze(K.map_fn(sample_length, K.arange(0, samples_left), dtype = "int32"), axis = -1)], axis = 0)   
+        
+        for length in range(self.low, self.high):
+            times_sampled = K.sum(K.cast(K.equal(all_lengths, length), "int32"))
+            
+            def sample_start(_):
+                return K.random_uniform(shape=(1,), minval=0, maxval=timesteps - length, dtype = "int32")
+            starts = K.squeeze(K.map_fn(sample_start, K.arange(times_sampled), dtype = "int32"), axis = -1)
+            
+            def sample_substring(start):
+                return inputs_padded[:,start:start+length]
+            
+            substrings = K.map_fn(sample_substring, starts, dtype = K.dtype(inputs))
+            # (samples, batchsize, length, ...)
+            substrings = K.permute_dimensions(substrings, [1,0] + list(range(2, K.ndim(substrings))))
+            substrings_by_size.append(substrings)
+
+            def sample_binary(start):
+                before = K.zeros((batchsize, start))
+                ones = K.ones((batchsize, length))
+                after = K.zeros((batchsize, timesteps - start - length))
+                return K.concatenate([before, ones, after], axis = 1)
+            
+            binaries = K.map_fn(sample_binary, starts, dtype = K.floatx())
+            # (samples, batchsize, timesteps)
+            binaries = K.permute_dimensions(binaries, [1,0,2])
+            # (batchsize, samples, timesteps)
+            binaries_by_size.append(binaries)
+
+            if mask:
+                def sample_submask(start):
+                    return mask_padded[:,start:start+length]
+
+                masks = K.map_fn(sample_submask, starts, dtype = K.dtype(mask))
+                # (samples, batchsize, length)
+                masks = K.permute_dimensions(masks, [1,0,2])
+                masks_by_size.append(masks)
+        
+        return substrings_by_size, binaries_by_size, masks_by_size
---
+++ perturbation_layers.py
@@ -0,0 +1,134 @@
+import keras.backend as K
+from explanation_layers import ExplanationLayer
+
+class InputPerturbation(ExplanationLayer):
+    def __init__(self, layer, size, axis, **kwargs):
+        super(InputPerturbation, self).__init__(layer, **kwargs)
+
+        if size < 1:
+            raise Exception("size must be > 0")
+
+        self.size = size
+        self.axis = axis
+
+
+class InputPerturbation1D(InputPerturbation):
+    def compute_output_shape(self, input_shape):
+        return input_shape[:2] + \
+                self.layer.compute_output_shape(self._compute_input_shape_int(input_shape))[1:]
+   
+    def call(self, inputs, mask = None):
+       
+        shape = K.shape(inputs) # (batch, timesteps, ...)
+        int_shape = K.int_shape(inputs)
+        timesteps = K.expand_dims(K.expand_dims(K.arange(0, K.shape(inputs)[self.axis]-self.size+1), -1), 0)
+
+        if int_shape[1]:
+            def _step(_, states):
+                start = states[0]
+                _inputs = self._perturb(inputs, start)
+                if mask is None:
+                    return self.layer.call(_inputs)
+                _mask = self._perturb_mask(mask, start)
+                return self.layer.call(_inputs, _mask)
+
+       
+            _, output, _ = K.rnn(_step, timesteps, initial_states = [0])
+        
+        else:
+            def _step(_, states):
+                [start, is_mask] = states
+                if is_mask:
+                    return self._perturb_mask(mask, start), [start+1]
+                return self._perturb(inputs, start), [start+1]
+
+            _, _inputs, _ = K.rnn(_step, timesteps, initial_states = [0], constants = [False])
+
+            # batchsize, timesteps - size + 1, _timesteps, [input_shape]
+            # omission: _timesteps = timesteps - size
+            # occlusion: _timesteps = timesteps
+
+            _input_shape = self._compute_input_shape(shape)
+            _reshape = K.concatenate([- K.ones((1,), dtype = K.dtype(_input_shape)), _input_shape[1:]], axis = 0)
+            _inputs = K.reshape(_inputs, _reshape)
+            # batchsize * (timesteps - size + 1), _timesteps, [input_shape]
+
+            _inputs._uses_learning_phase = inputs._uses_learning_phase
+            _inputs._keras_shape = (None, None) + int_shape[2:]
+
+            if not mask is None:
+                _, _mask, _ = K.rnn(_step, timesteps, initial_states = [0], constants = [True])
+                _mask_reshape = K.concatenate([- K.ones((1,), dtype = K.dtype(_input_shape)), _input_shape[1:2]], axis = 0)
+                _mask = K.reshape(_mask, _mask_reshape)
+                _output = self.layer.call(_inputs, mask = _mask)
+            else:
+                _output = self.layer.call(_inputs)
+                # (batchsize * (timesteps - size + 1), [output_shape])
+
+            
+            output_shape = self.compute_output_shape(int_shape)
+
+            if output_shape[1] is None:
+                if output_shape[2] is None:
+                    dummy_input = self._perturb(inputs, 0)
+                    dummy_output = self.layer.call(dummy_input)
+                    return K.reshape(_output, (-1, shape[self.axis]-self.size+1, K.shape(dummy_output)[1]) + output_shape[3:])
+                return K.reshape(_output, (-1, shape[self.axis]-self.size+1) + output_shape[2:])
+            return K.reshape(_output, (-1,) + output_shape[1:])
+    
+class InputOmission1D(InputPerturbation1D):
+    def __init__(self, layer, size=1, **kwargs):
+        super(InputOmission1D, self).__init__(layer, size, axis=1, **kwargs)
+
+    def _perturb_mask(self, mask, start):
+        if self.axis > 1:
+            return mask
+        _mask = mask
+        _mask = K.permute_dimensions(_mask, [self.axis, 1-self.axis])
+        _mask = K.concatenate([_mask[:start], _mask[start+self.size:]], axis = 0)
+        _mask = K.permute_dimensions(_mask, [self.axis, 1-self.axis])
+        return _mask
+
+    def _perturb(self, x, start):
+        _x = x
+        _x = K.permute_dimensions(_x, [self.axis] + [i if i != self.axis else 0 for i in range(1, K.ndim(x))])
+        _x = K.concatenate([_x[:start], _x[start+self.size:]], axis = 0)
+        _x = K.permute_dimensions(_x, [self.axis] + [i if i != self.axis else 0 for i in range(1, K.ndim(x))])
+        
+        _shape = K.int_shape(x)
+        if _shape[self.axis]:
+            _shape = tuple([_shape[i] if i != self.axis else _shape[i] - self.size for i in range(len(_shape))])
+        
+        _x._keras_shape = _shape
+        _x._uses_learning_phase = x._uses_learning_phase
+        return _x
+    
+    def _compute_input_shape_int(self, input_shape):
+        if input_shape[self.axis]:
+            return tuple([_shape[i] if i != self.axis else _shape[i] - self.size for i in range(len(_shape))])
+        return input_shape
+    
+    def _compute_input_shape(self, input_shape):
+        cond = K.equal(K.arange(0, K.shape(input_shape)[0]), self.axis)
+        return K.switch(cond, input_shape - self.size, input_shape)
+
+class InputOcclusion1D(InputPerturbation1D):
+    def _perturb_mask(self, mask, start):
+        return mask
+
+    def _perturb(self, x, start):
+        _x = x
+        _x = K.permute_dimensions(_x, [self.axis] + [i if i != self.axis else 0 for i in range(1, K.ndim(x))])
+        _x = K.concatenate([_x[:start], K.zeros_like(_x[:self.size]), _x[start+self.size:]], axis = 0)
+        _x = K.permute_dimensions(_x, [self.axis] + [i if i != self.axis else 0 for i in range(1, K.ndim(x))])
+
+        _x._keras_shape = x._keras_shape
+        _x._uses_learning_phase = x._uses_learning_phase
+        return _x
+    
+
+    def _compute_input_shape_int(self, input_shape):
+        return input_shape
+    
+    def _compute_input_shape(self, input_shape):
+        return input_shape
